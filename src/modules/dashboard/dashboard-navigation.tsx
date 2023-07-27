f69f9b81cdd8a0d02134495934b56714f3e0447f
Radosław Rzepka <radoslawrzepka02@gmail.com>
2023-07-27 22:38:23 +0200

    dashboard navigation styling

--- src/modules/dashboard/dashboard-navigation.tsx
+++ src/modules/dashboard/dashboard-navigation.tsx
@@ -5,16 +5,19 @@ import LogoText from "@/components/ui/logo-text";
 import Link from "next/link";
 import Button from "@/components/ui/button";
 import { signOut } from "next-auth/react";
+import { useRouter } from "next/navigation";
 
 interface DashboardNavigationProps {}
 
 const DashboardNavigation: FC<DashboardNavigationProps> = ({}) => {
+   const router = useRouter();
+
    return (
-      <nav className="m-6 flex items-center justify-between rounded-xl bg-text p-4 text-background">
+      <nav className="m-6 flex flex-col items-center justify-between gap-4 rounded-xl bg-text p-4 text-background xl:flex-row">
          <LogoText variant="dark">
             <Link href="/dashboard">Quizzler</Link>
          </LogoText>
-         <ul className="flex gap-6">
+         <ul className="flex gap-6 text-center">
             <li>
                <Link href="/">New lesson</Link>
             </li>
@@ -29,12 +32,21 @@ const DashboardNavigation: FC<DashboardNavigationProps> = ({}) => {
                <Link href="/">New flashcard</Link>
             </li>
          </ul>
-         <Button
-            label="Sign out"
-            variant="accent"
-            type="button"
-            onClick={() => signOut()}
-         />
+         <div className="grid place-items-center lg:block">
+            <Button
+               label="My profile"
+               variant="black"
+               type="button"
+               onClick={() => router.push("/")}
+               className="mb-3 lg:mb-0 lg:mr-3"
+            />
+            <Button
+               label="Sign out"
+               variant="accent"
+               type="button"
+               onClick={() => signOut()}
+            />
+         </div>
       </nav>
    );
 };
