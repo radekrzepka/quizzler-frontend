72cb86b18e0354f6e540525546fc6d886a5e6166
Radosław Rzepka <radoslawrzepka02@gmail.com>
2023-08-06 00:57:19 +0200

    feat: handling login errors

--- src/modules/auth/sign-in/sign-in-form-schema.ts
+++ src/modules/auth/sign-in/sign-in-form-schema.ts
@@ -1,10 +1,11 @@
 import { z } from "zod";
 
 export const signInFormSchema = z.object({
-   usernameOrEmail: z
+   email: z
       .string()
-      .min(1, { message: "Email/username is required" })
-      .max(100, { message: "Email/username too long (max 100 characters)" }),
+      .email()
+      .min(1, { message: "Email is required" })
+      .max(100, { message: "Email too long (max 100 characters)" }),
    password: z
       .string()
       .min(1, { message: "Password is required" })
--- src/modules/auth/sign-in/sign-in-form.tsx
+++ src/modules/auth/sign-in/sign-in-form.tsx
@@ -10,6 +10,7 @@ import ErrorMessage from "@/components/ui/error-message";
 import { useRouter } from "next/navigation";
 import { useMutation } from "@tanstack/react-query";
 import { useState } from "react";
+import Modal from "@/components/ui/modal";
 
 const SignInForm = () => {
    const {
@@ -23,19 +24,20 @@ const SignInForm = () => {
 
    const router = useRouter();
    const [buttonLoading, setButtonLoading] = useState(false);
+   const [openErrorModal, setOpenErrorModal] = useState(false);
 
-   const { usernameOrEmail, password } = getValues();
+   const { email, password } = getValues();
 
    const { mutate: loginUserMutation, data: response } = useMutation({
       mutationFn: () => {
-         return fetch(`/api/login-user`, {
+         return fetch(`/api/user/login`, {
             headers: {
                Accept: "application/json",
                "Content-Type": "application/json",
             },
             method: "POST",
             body: JSON.stringify({
-               usernameOrEmail,
+               email,
                password,
             }),
          });
@@ -43,6 +45,9 @@ const SignInForm = () => {
 
       onSettled: (res) => {
          if (res?.status === 200) router.push("/dashboard");
+         else if (res?.status !== 409 && res?.status !== 400) {
+            setOpenErrorModal(true);
+         }
          setButtonLoading(false);
       },
    });
@@ -57,17 +62,47 @@ const SignInForm = () => {
          onSubmit={handleSubmit(onSubmit)}
          className="rounded-md bg-[#f1f1f1] p-6 text-background md:p-10"
       >
+         {openErrorModal && (
+            <Modal
+               closeModalFunction={() => {
+                  setOpenErrorModal(false);
+                  setButtonLoading(false);
+               }}
+            >
+               <div className="text-center">
+                  <p className="mb-3">
+                     We apologize for the inconvenience, but it seems that
+                     something is not quite right with our server at the moment.
+                     We are aware of the issue and are actively working to
+                     resolve it as quickly as possible.
+                  </p>
+                  <p className="mb-3">
+                     Please bear with us while we fix the problem. We appreciate
+                     your patience and understanding.
+                  </p>
+                  <Button
+                     type="button"
+                     variant="primary"
+                     label="Close"
+                     onClick={() => {
+                        setOpenErrorModal(false);
+                        setButtonLoading(false);
+                     }}
+                  />
+               </div>
+            </Modal>
+         )}
          <LabelInputContainer>
-            <label htmlFor="usernameOrEmail">Enter email/username: </label>
+            <label htmlFor="email">Enter email: </label>
             <TextInput
-               id="usernameOrEmail"
+               id="email"
                type="text"
                register={register}
-               name="usernameOrEmail"
-               className={!errors.usernameOrEmail ? "mb-7" : ""}
+               name="email"
+               className={!errors.email ? "mb-[23px]" : ""}
             />
-            {errors.usernameOrEmail && (
-               <ErrorMessage>{errors.usernameOrEmail.message}</ErrorMessage>
+            {errors.email && (
+               <ErrorMessage>{errors.email.message}</ErrorMessage>
             )}
          </LabelInputContainer>
          <LabelInputContainer>
@@ -78,14 +113,22 @@ const SignInForm = () => {
                register={register}
                name="password"
                className={
-                  !errors.password && !(response?.status === 400) ? "mb-7" : ""
+                  !errors.password &&
+                  !(response?.status === 400 || response?.status === 409)
+                     ? "mb-[23px]"
+                     : ""
                }
             />
             {errors.password && (
                <ErrorMessage>{errors.password.message}</ErrorMessage>
             )}
             {response?.status === 400 && (
-               <ErrorMessage>Incorrect login or password</ErrorMessage>
+               <ErrorMessage> Wrong credentials</ErrorMessage>
+            )}
+            {response?.status === 409 && (
+               <ErrorMessage>
+                  There is no account set up with the given email
+               </ErrorMessage>
             )}
          </LabelInputContainer>
          <div className="flex flex-col justify-between md:flex-row md:gap-3">
