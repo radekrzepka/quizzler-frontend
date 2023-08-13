da9cfc7e58a37bcce1e6701475f7fe935340d0e7
Radosław Rzepka <radoslawrzepka02@gmail.com>
2023-08-14 00:40:48 +0200

    fix: fix error with modal

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,12 +1,10 @@
-"use client";
-
-import Modal from "@/components/ui/modal";
-
 export default function NotFound() {
    return (
-      <Modal displayCloseIcon={false}>
-         <p className="text-4xl font-bold">404 error - Not Found</p>
-         <p className="text-base">Could not find requested resource</p>
-      </Modal>
+      <div className="grid h-screen w-screen place-items-center">
+         <div className="w-3/4 rounded-xl bg-text p-4 py-10 text-center text-black">
+            <p className="text-4xl font-bold">404 error - Not Found</p>
+            <p className="text-base">Could not find requested resource</p>
+         </div>
+      </div>
    );
 }
