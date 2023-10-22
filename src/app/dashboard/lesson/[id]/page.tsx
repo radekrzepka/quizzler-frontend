f61470f0f1e1239d329903c325aafd4a418ce8c2
ziebamikolaj <ziebamikolaj03@gmail.com>
2023-10-22 19:58:07 +0200

    fix: resolving merge conflicts

--- src/app/dashboard/lesson/[id]/page.tsx
+++ src/app/dashboard/lesson/[id]/page.tsx
@@ -1,42 +1,5 @@
-"use client";
-
-import EditLesson from "@/modules/lesson/edit-lesson";
-import { Lesson } from "@/types/lesson";
-import { useQuery } from "@tanstack/react-query";
-import { getCookie } from "cookies-next";
-
-const getLesson = async (id: string) => {
-   const JWT = getCookie("JWT") as string;
-   const res = await fetch(`/api/lesson/${id}`, {
-      headers: { Authorization: JWT },
-   });
-
-   if (!res.ok) {
-      throw new Error("Failed to fetch data");
-   }
-
-   const { data } = await res.json();
-
-   return data;
-};
-
-const Lesson = ({
-   params,
-   searchParams,
-}: {
-   params: { id: string };
-   searchParams: { [key: string]: string };
-}) => {
-   const { data: lesson, refetch } = useQuery<Lesson>({
-      queryKey: ["lesson", params.id],
-      queryFn: () => getLesson(params.id),
-   });
-   const editLesson = searchParams?.edit === "true";
-
-   if (editLesson && lesson)
-      return <EditLesson lesson={lesson} refetchLesson={refetch} />;
-
-   return <div></div>;
+const LessonPage = ({ params }: { params: { id: string } }) => {
+   return <div>Learning page</div>;
 };
 
 export default LessonPage;
