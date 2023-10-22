910ca287c81b8685107babf6f8642e665add35ba
ziebamikolaj <ziebamikolaj03@gmail.com>
2023-10-22 22:43:57 +0200

    feat: button interactive color, better date format

--- src/modules/my-profile/profile-card.tsx
+++ src/modules/my-profile/profile-card.tsx
@@ -1,13 +1,15 @@
 "use client";
 
 import { UserInfo } from "@/types/user-info";
-
 import LineChart from "@/components/chart/line-chart";
 import ChartDateFilters from "@/components/chart/chart-date-filters";
 import {
-  subDays,
-  format,
-  parseISO,
+   subDays,
+   format,
+   parseISO,
+   subMonths,
+   isEqual,
+   addDays,
 } from "date-fns";
 import { ChartRecord } from "@/types/chart-data";
 import Image from "next/image";
@@ -18,29 +20,36 @@ import classNames from "classnames";
 import { generateAbbreviation } from "@/utils/generate-abbreviation";
 import ChangeAvatar from "./change-avatar";
 
-
 interface ProfileCardProps {
-  profile: UserInfo;
-  createdDates: Date[];
-  learnedDates: LogData[];
+   profile: UserInfo;
+   createdDates: Date[];
+   learnedDates: LogData[];
 }
 
-const today = new Date();
-
 const generateEmptyDates = () => {
-  const dates: ChartRecord[] = [];
-  for (let i = 334; i >= 0; i--) {
-    dates.push({ name: format(subDays(today, i), "dd MMMM yyyy"), value: 0});
-  }
-  return dates;
+   const today = new Date();
+   const dates: ChartRecord[] = [];
+   const startDate = subMonths(today, 11);
+   let procDate = startDate;
+   do {
+      dates.push({
+         name: format(procDate, "dd MMMM yyyy"),
+         value: 0,
+      });
+      procDate = addDays(procDate, 1);
+   } while (!isEqual(procDate, today));
+
+   return dates;
 };
 
 const datesChartFormat = (data: Date[], emptyDates: ChartRecord[]) => {
-   data.forEach((date) =>{
+   data.forEach((date) => {
       date = new Date(date);
-      const dateInEmptyDates = emptyDates.find(ele => ele.name == format(date, "dd MMMM yyyy"));    
-      if(dateInEmptyDates) dateInEmptyDates.value+=1;
-      else emptyDates.push({name: format(date, "dd MMMM yyyy"), value: 1});
+      const dateInEmptyDates = emptyDates.find(
+         (ele) => ele.name == format(date, "dd MMMM yyyy"),
+      );
+      if (dateInEmptyDates) dateInEmptyDates.value += 1;
+      else emptyDates.push({ name: format(date, "dd MMMM yyyy"), value: 1 });
    });
    return emptyDates;
 };
@@ -49,67 +58,89 @@ const reduceData = (data: ChartRecord[]) => {
    const result: ChartRecord[] = [];
    const recordsLength = data.length;
    // Determine formatting option based on the length of records.
-   let formattingOption = recordsLength >= 90 ? "LLLL" : recordsLength >= 28 ? "PP" : "do";
+   let formattingOption =
+      recordsLength >= 90 ? "LLLL yyyy" : recordsLength >= 28 ? "PP" : "do MMM";
 
-   if(formattingOption !== "PP"){
+   if (formattingOption !== "PP") {
       data.forEach((record) => {
-         const dateInResult = result.find(item => item.name === format(new Date(record.name), formattingOption));
-         if(dateInResult) dateInResult.value += record.value;
-         else result.push({name: format(new Date(record.name), formattingOption), value: 0});
-      })
-   }
-   else {
+         const processDate = new Date(record.name);
+         const dateInResult = result.find(
+            (item) => item.name === format(processDate, formattingOption),
+         );
+         if (dateInResult) dateInResult.value += record.value;
+         else
+            result.push({
+               name: format(processDate, formattingOption),
+               value: 0,
+            });
+      });
+   } else {
       let sumForTheWeek = 0;
-      data.forEach((record, index) =>{
+      data.forEach((record, index) => {
          sumForTheWeek += record.value;
          const isLastRecord = index === data.length - 1;
          const isSunday = format(new Date(record.name), "EEEE") === "Sunday";
 
          if (isSunday || isLastRecord) {
-           const date = new Date(record.name);
-           const stringDates = `${format(subDays(date, 6), "d")} ${format(subDays(date, 6), "MMM")} - ${format(date, "d")} ${format(date, "MMM")}`;
-           result.push({ name: stringDates, value: sumForTheWeek });
-           sumForTheWeek = 0; // reset for the next week
+            const date = new Date(record.name);
+            const stringDates = `${format(subDays(date, 6), "d")} ${format(
+               subDays(date, 6),
+               "MMM",
+            )} - ${format(date, "d")} ${format(date, "MMM")} ${format(
+               date,
+               "yyyy",
+            )}`;
+            result.push({ name: stringDates, value: sumForTheWeek });
+            sumForTheWeek = 0; // reset for the next week
          }
-      })
+      });
    }
    return result;
- };
- 
-const ProfileCard: FC<ProfileCardProps> = ({ profile, createdDates, learnedDates }) => {
-  const emptyDatesCreated = generateEmptyDates();
-  const emptyDatesLearned = generateEmptyDates();
+};
 
-  const createdFlashcards = useMemo(() => {
-    return datesChartFormat(createdDates, emptyDatesCreated)
-  }, [emptyDatesCreated, createdDates]);
+const ProfileCard: FC<ProfileCardProps> = ({
+   profile,
+   createdDates,
+   learnedDates,
+}) => {
+   const emptyDatesCreated = generateEmptyDates();
+   const emptyDatesLearned = generateEmptyDates();
 
-  const learnedFlashcards = useMemo(() => {
-    return datesChartFormat(
-      learnedDates.filter(log => log.wasCorrect).map((log) => log.date),
-      emptyDatesLearned
-    )
-  }, [emptyDatesLearned, learnedDates]);
+   const createdFlashcards = useMemo(() => {
+      return datesChartFormat(createdDates, emptyDatesCreated);
+   }, [emptyDatesCreated, createdDates]);
 
-  const firstCreatedDate = format(new Date(createdFlashcards.find(stat => stat.value !== 0)?.name || createdFlashcards[0].name), "dd MMMM yyyy");
-  const firstLearnedDate = format(new Date(learnedFlashcards.find(stat => stat.value !== 0)?.name || learnedFlashcards[0].name), "dd MMMM yyyy");
+   const learnedFlashcards = useMemo(() => {
+      return datesChartFormat(
+         learnedDates.filter((log) => log.wasCorrect).map((log) => log.date),
+         emptyDatesLearned,
+      );
+   }, [emptyDatesLearned, learnedDates]);
 
-  const [createdStartDate, setCreatedStartDate] = useState(new Date(firstCreatedDate));
-  const [learnedStartDate, setLearnedStartDate] = useState(new Date(firstLearnedDate));
+   const [createdStartDate, setCreatedStartDate] = useState(
+      new Date(parseISO(profile.dateRegistered)),
+   );
+   const [learnedStartDate, setLearnedStartDate] = useState(
+      new Date(parseISO(profile.dateRegistered)),
+   );
 
-  const selectedCreatedFlashcards = useMemo(() => {
-    return reduceData(createdFlashcards.slice().filter((record) =>
-      new Date(record.name) >= createdStartDate )
-    );
-  }, [createdStartDate, createdFlashcards]);
+   const selectedCreatedFlashcards = useMemo(() => {
+      return reduceData(
+         createdFlashcards
+            .slice()
+            .filter((record) => new Date(record.name) >= createdStartDate),
+      );
+   }, [createdStartDate, createdFlashcards]);
 
-  const selectedLearnedFlashcards = useMemo(() => {
-    return reduceData(learnedFlashcards.slice().filter((record) =>
-      new Date(record.name) >= learnedStartDate )
-    );
-  }, [learnedStartDate, learnedFlashcards]);
+   const selectedLearnedFlashcards = useMemo(() => {
+      return reduceData(
+         learnedFlashcards
+            .slice()
+            .filter((record) => new Date(record.name) >= learnedStartDate),
+      );
+   }, [learnedStartDate, learnedFlashcards]);
 
-  const [isOpen, setIsOpen] = useState(false);
+   const [isOpen, setIsOpen] = useState(false);
 
    return (
       <div className="flex flex-col items-center rounded-xl bg-text text-background">
@@ -177,10 +208,7 @@ const ProfileCard: FC<ProfileCardProps> = ({ profile, createdDates, learnedDates
             changeStartDate={setLearnedStartDate}
             registerDate={parseISO(profile.dateRegistered)}
          />
-         <LineChart
-            data={selectedLearnedFlashcards}
-            id="Lessons stats chart"
-         />
+         <LineChart data={selectedLearnedFlashcards} id="Lessons stats chart" />
       </div>
    );
 };
