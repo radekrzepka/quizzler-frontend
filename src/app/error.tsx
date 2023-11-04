8214f18db0eee180bbb2f17c62e5eb0dca88b255
Radosław Rzepka <radoslawrzepka02@gmail.com>
2023-11-04 20:48:17 +0100

    feature: change stling for error modal

--- src/app/error.tsx
+++ src/app/error.tsx
@@ -4,19 +4,19 @@ import Button from "@/components/ui/button";
 
 export default function Error({ reset }: { reset: () => void }) {
    return (
-      <div className="fixed left-0 top-0 grid h-screen w-screen place-items-center bg-background bg-opacity-50 text-background">
-         <div className="relative z-20 grid w-4/5 place-items-center rounded-xl bg-text p-5 lg:w-1/2">
+      <div className="fixed left-0 top-0 grid h-screen w-screen place-items-center bg-background text-background">
+         <div className="grid w-4/5 place-items-center rounded-xl bg-text p-5 lg:w-1/2">
             <div className="grid place-items-center text-center">
-               <h2 className="mb-3">
+               <p className="mb-3 text-lg">
                   An error occurred. Please try again later. If the problem
                   persists, contact support.
-               </h2>
+               </p>
 
                <Button
                   type="button"
                   variant="primary"
                   label="Try again"
-                  onClick={() => reset()}
+                  onClick={reset}
                />
             </div>
          </div>
