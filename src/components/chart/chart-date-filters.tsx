cf322725e1e13d02a67c42352a4c3a8874504d3d
ziebamikolaj <ziebamikolaj03@gmail.com>
2023-11-13 22:44:15 +0100

    feat: chart tweaks

--- src/components/chart/chart-date-filters.tsx
+++ src/components/chart/chart-date-filters.tsx
@@ -1,5 +1,5 @@
 import { subDays } from "date-fns";
-import type { Dispatch, SetStateAction} from "react";
+import type { Dispatch, SetStateAction } from "react";
 import { useState } from "react";
 import classNames from "classnames";
 interface ChartDateFiltersProps {
@@ -12,14 +12,14 @@ const ChartDateFilters = ({
    registerDate,
 }: ChartDateFiltersProps) => {
    const today = new Date();
-   const [selectedButton, setSelectedButton] = useState(4);
+   const [selectedButton, setSelectedButton] = useState(1);
    const formattingOption = "bg-background text-white";
    return (
       <div className="my-2 flex flex-wrap items-center justify-center gap-4">
          <button
             className={classNames(
                "rounded-md bg-accent p-1 px-6 text-background",
-               selectedButton == 1 ? formattingOption : "",
+               selectedButton == 1 ? formattingOption : ""
             )}
             onClick={() => {
                changeStartDate(subDays(today, 7));
@@ -31,7 +31,7 @@ const ChartDateFilters = ({
          <button
             className={classNames(
                "rounded-md bg-accent p-1 px-6 text-background",
-               selectedButton == 2 ? formattingOption : "",
+               selectedButton == 2 ? formattingOption : ""
             )}
             onClick={() => {
                changeStartDate(subDays(today, 31));
@@ -43,7 +43,7 @@ const ChartDateFilters = ({
          <button
             className={classNames(
                "rounded-md bg-accent p-1 px-6 text-background",
-               selectedButton == 3 ? formattingOption : "",
+               selectedButton == 3 ? formattingOption : ""
             )}
             onClick={() => {
                changeStartDate(subDays(today, 365));
@@ -55,7 +55,7 @@ const ChartDateFilters = ({
          <button
             className={classNames(
                "rounded-md bg-accent p-1 px-6 text-background",
-               selectedButton == 4 ? formattingOption : "",
+               selectedButton == 4 ? formattingOption : ""
             )}
             onClick={() => {
                changeStartDate(registerDate);
